f98aa95f1ea8ffd47b843451d502aaf389d96160
yego5 <shachz.github@gmail.com>
2023-03-16 17:06:04 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#inlude <stdio.h>
+
+/**
+*main - put the string in the main function 
+*
+*Descripion: using the main function 
+*this program prints with proper grammar, but the outcome is a piece of art,
+*Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
