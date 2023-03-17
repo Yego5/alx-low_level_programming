3e997d8dbef9854c4a84484102603362f99a5103
yego5 <shachz.github@gmail.com>
2023-03-16 19:27:19 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,14 @@
-git status
+#include <stdio.h>
+
+/**
+*main - print the string in the put function
+*
+*Description: using the main function
+*this programming prints "Programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main(void)
+{
+	puts ("\""Programming is like building a multilingual puzzle");
+	return (0);
+}
