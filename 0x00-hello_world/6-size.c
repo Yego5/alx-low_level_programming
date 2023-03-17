a505a89fdf534b695a07f74c2ab9f6517a8b240e
yego5 <shachz.github@gmail.com>
2023-03-16 18:36:35 -0700

    note changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
+#include <stdio.h>                            	 
 
 /**
-*main - print the main string in the put function 
+*main - print the main string in the put function
 *
-*Description: using the main function 
+*Description: using the main function
 *this programe will orint "the size of various types on the computer it is compiled and run on.
 
 *Return 0
 */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
-	
-	printf("size of a char: %id byte(s)\n", sizeof(c));
-	printf("size of an int: %id byte(s)\n", sizeof(i));
-	printf("size of a long int: %id byte(s)\n", sizeof(li));
-	printf("size of a long long int: %id byte(s)\n", sizeof(lli);
-	printf("size of a float: %id byte(s)\n", sizeof(f));
-	return (0);
+    	char c;
+    	int i;
+    	long li;
+    	long long lli;
+    	float f;
+
+    	Printf ("size of a char: %id byte(s)\n", sizeof(c));
+    	Printf ("size of an int: %id byte(s)\n", sizeof(i));
+    	Printf ("size of a long int: %id byte(s)\n", sizeof(li));
+    	Printf ("size of a long long int: %id byte(s)\n", sizeof(lli);
+    	Printf ("size of a float: %id byte(s)\n", sizeof(f));
+    	return (0);
 }
