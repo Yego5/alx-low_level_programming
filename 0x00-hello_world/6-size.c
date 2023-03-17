1a6a5782cf0141490565ea0bb91118b242101f90
yego5 <shachz.github@gmail.com>
2023-03-17 10:31:22 -0700

    correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
+
 /**
-*main - print the string in the put function
-*
-*Description: using the main function
-*this program prints "programming is like building a multilingual puzzle
-*Return 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    	char c;
-    	int i;
-    	long li;
-    	long long lli;
-    	float f;
-
-    	printf ("size of a char: %id byte(s)\n", sizeof(c));
-    	printf ("size of an int: %id byte(s)\n", sizeof(i));
-    	printf ("size of a long: %id byte(s)\n", sizeof(li));
-    	printf ("size of a long long: %id byte(s)\n", sizeof(lli));
-    	printf ("size of a float: %id byte(s)\n", sizeof(f));
-    	return (0);
+    printf("Size of a char: %lu byte(s)\n", sizeof(char));
+    printf("Size of an int: %lu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+    printf("Size of a float: %lu byte(s)\n", sizeof(float));
+    return (0);
 }
