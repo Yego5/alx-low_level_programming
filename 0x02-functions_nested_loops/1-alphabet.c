3c010fe8e57884014c1df494cf9bf959cf0cb84a
yego5 <shachz.github@gmail.com>
2023-03-21 13:00:48 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet of letters
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x<= 'z'; x++)
+
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
