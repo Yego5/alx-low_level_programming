50e669501adfa202b5b3fc7f81920e6279e1d39b
yego5 <shachz.github@gmail.com>
2023-04-03 14:02:20 -0700

    1-memcpy.c2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,15 +7,15 @@
  *
  *Return: copied memory with n bytes changed
  */
-char* _memcpy(char* dest, const char* src, size_t n) {
-	char* pDest = dest;
-	const char* pSrc = src;
-	while (n-- > 0) {
-    	*pDest++ = *pSrc++;
-	}
-	return dest;
-}
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int x = 0;
+	int y = n;
 
-char* memcpy_custom(char* dest, char* src, unsigned int n) {
-	return memcpy_custom(dest, (const char*) src, n);
+	for (; x < y; x++)
+	{
+		dest[x] = src[x];
+		n--;
+	}
+	return (dest);
 }
