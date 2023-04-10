7a532de1984487ab35d3b2e10dabb5ad8a5ea8b3
yego5 <shachz.github@gmail.com>
2023-04-10 14:54:54 -0700

    libmy.a

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+nt _putchar(char c)
+{
+	    return putchar(c);
+}
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main(void)
+{
+	    _puts("\"At the end, my goal is to be the best web developer\"\n\t- Shadrack Kimutai Yego");
+	        return (0);
+}
