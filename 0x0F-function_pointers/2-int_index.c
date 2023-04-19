f4ff4320f5440d69dc126a1b9fdf3f44e238357d
yego5 <shachz.github@gmail.com>
2023-04-19 15:29:17 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - return index plc if it's = true, else -1
+ * @array: array
+ * @size: size of elems in array
+ * @cmp: pointer to func of one of 3 in main
+ *
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+
+{
+	Int n;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (n = 0; n < size; n++)
+	{
+		if (cmp(array[n]))
+			return (n);
+	}
+	return (-1);
+}
