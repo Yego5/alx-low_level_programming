247c6390865403f314684658e0bc8f36c3feaf5d
yego5 <shachz.github@gmail.com>
2023-04-27 13:41:43 -0700

    0-print_list.c4

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t k = 0;
+	size_t s = 0;
 
-    while (h)
-    {
-   	 if (!h->str)
-   		 printf("[0] (nil)\n");
-   	 else
-   		 printf("[%u] %k\n", h->len, h->str);
-   	 h = h->next;
-   	 k++;
-    }
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
 
-    return (s);
+	return (s);
 }
