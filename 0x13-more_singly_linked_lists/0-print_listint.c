21a36c4d0b8fd81a286d7381437d1d3d3d5370e7
yego5 <shachz.github@gmail.com>
2023-05-02 15:16:08 -0700

    the correction on task 0-print_list.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,22 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in linked lists
- * @h: linked list of type listint_t to traverse
+ * print_listint - prints all elements of linked list
+ * @h: linked list of type listint_t to print
  *
- * Return: returns the number of nodes
+ * Return:to return the number of nodes
  */
-size_t listint_len(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
 	size_t nam = 0;
 
 	while (h)
 	{
-		nam++;
-		h = h->next;
+		printf("%d\n", h->n);
+			h = h->next;
+nam++;
 	}
 
 	return (nam);
 }
+
