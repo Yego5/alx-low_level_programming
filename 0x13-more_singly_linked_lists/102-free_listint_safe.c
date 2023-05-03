8db4199f45ca920f9e5ef616e5f9a832b9021aaa
yego5 <shachz.github@gmail.com>
2023-05-02 19:32:22 -0700

    102-free_listint_safe.c1

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,33 +8,33 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
- size_t l = 0;
- int diff;
- listint_t *temp;
+size_t l = 0;
+int diff;
+listint_t *temp;
 
- if (!h || !*h)
- return (0);
+if (!h || !*h)
+return (0);
 
- while (*h)
- {
- diff = *h - (*h)->next;
- if (diff > 0)
- {
- temp = (*h)->next;
- free(*h);
- *h = temp;
- l++;
- }
- else
- {
- free(*h);
- *h = NULL;
- l++;
- break;
- }
- }
+while (*h)
+{
+diff = *h - (*h)->next;
+if (diff > 0)
+{
+temp = (*h)->next;
+free(*h);
+*h = temp;
+l++;
+}
+else
+{
+free(*h);
+*h = NULL;
+l++;
+break;
+}
+}
 
- *h = NULL;
+*h = NULL;
 
- return (l);
+return (l);
 }
