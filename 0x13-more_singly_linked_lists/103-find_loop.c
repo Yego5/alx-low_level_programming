ba30a3c99887f8a57df4dc561c6a260073f3d429
yego5 <shachz.github@gmail.com>
2023-05-02 19:29:27 -0700

    103-find_loop.c1

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,27 +8,27 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
- listint_t *slw = head;
- listint_t *fst = head;
+listint_t *slw = head;
+listint_t *fst = head;
 
- if (!head)
- return (NULL);
+if (!head)
+return (NULL);
 
- while (slw && fst && fst->next)
- {
- fst = fst->next->next;
- slw = slw->next;
- if (fst == slw)
- {
- slw = head;
- while (slw != fst)
- {
- slw = slw->next;
- fst = fst->next;
- }
- return (fst);
- }
- }
+while (slw && fst && fst->next)
+{
+fst = fst->next->next;
+slw = slw->next;
+if (fst == slw)
+{
+slw = head;
+while (slw != fst)
+{
+slw = slw->next;
+fst = fst->next;
+}
+return (fst);
+}
+}
 
- return (NULL);
+return (NULL);
 }
