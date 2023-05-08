e0bab89c08eee5db7ef7940588e12698fa74a5e0
yego5 <shachz.github@gmail.com>
2023-05-08 16:40:11 -0700

    0-read_textfile.c1 task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,45 +10,38 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-char *buf;
-ssize_t bytes_read, ret;
+	if (filename == NULL)
+		return (0);
 
-if (!filepath)
-return (0);
+	int fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-fd = open(filepath, O_RDONLY);
-if (fd == -1)
-{
-close(fd);
-return (0);
-}
+	char *buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-buf = malloc(sizeof(char) * num_chars);
-if (!buf)
-{
-close(fd);
-return (0);
-}
+	ssize_t bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
 
-bytes_read = read(fd, buf, num_chars);
+	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
 
-if (bytes_read == -1)
-{
-close(fd);
-free(buf);
-return (0);
-}
-
-ret = write(STDOUT_FILENO, buf, bytes_read);
-
-if (ret == -1)
-{
-close(fd);
-free(buf);
-return (0);
-}
-close(fd);
-return (bytes_read);
+	close(fd);
+	free(buffer);
+	return (bytes_written);
 }
 
