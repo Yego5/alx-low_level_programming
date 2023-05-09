67f5c15bc9d46efb6244b676700ee8702e7b2306
yego5 <shachz.github@gmail.com>
2023-05-08 17:13:29 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,21 +9,38 @@
  *        0 when the function fails or the filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
-{
-	char *buff;
-	ssize_t fld;
-	ssize_t wr;
-	ssize_t t;
-
-	fld = open(filename, O_RDONLY);
-	if (fld == -1)
+	{
+	if (filename == NULL)
 		return (0);
-	buff = malloc(sizeof(char) * letters);
-	t = read(fld, buff, letters);
-	Wr = write(STDOUT_FILENO, buff, t);
 
-	free(buff);
-	close(fld);
-	return (wr);
+	int fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	char *buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	ssize_t bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	close(fd);
+	free(buffer);
+	return (bytes_written);
 }
-
