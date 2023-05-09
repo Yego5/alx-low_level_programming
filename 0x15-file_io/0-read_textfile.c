1fb3814b4aeb6131c9e182c5668902b22a981161
yego5 <shachz.github@gmail.com>
2023-05-08 17:54:53 -0700

    0-read_textfile.c task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	buff = malloc(sizeof(char) * letters);
 	t = read(fld, buff, letters);
-	Wr = write(STDOUT_FILENO, buff, t);
+	wr = write(STDOUT_FILENO, buff, t);
 
 	free(buff);
 	close(fld);
