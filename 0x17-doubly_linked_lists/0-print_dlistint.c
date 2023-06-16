eded300228ccf1b18a06959b1c61714c5576a39c
yego5 <shachz.github@gmail.com>
2023-06-16 12:51:27 -0700

    0-print_dlistint.c1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,21 +10,16 @@
 size_t print_dlistint(const dlistint_t *hd)
 {
     int coun;
-
     coun = 0;
-
     if (hd == NULL)
         return coun;
-
     while (hd->prev != NULL)
         hd = hd->prev;
-
     while (hd != NULL)
     {
         printf("%d\n", hd->n);
         coun++;
         hd = hd->next;
     }
-
     return coun;
 }
