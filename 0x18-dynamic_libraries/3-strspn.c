e4f9a06c673842ab566b2f464c2b9fa6f082cf26
yego5 <shachz.github@gmail.com>
2023-06-19 17:04:40 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x = 0;
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+			{
+				x++;
+				break;
+			}
+			else if (accept[k + 1] == '\0')
+				return (x);
+		}
+		s++;
+	}
+	return (x);
+}
