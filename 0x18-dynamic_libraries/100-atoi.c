e71e53b9152fbd2cb64f5f0a986e2ec9ca6f5b2d
yego5 <shachz.github@gmail.com>
2023-06-20 14:11:52 -0700

    100-atoi.c update

--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -1,5 +1,3 @@
-#include "main.h"
-
 /**
  * _atoi - converts a string to an integer
  * @str: string to be converted
@@ -8,39 +6,39 @@
  */
 int _atoi(char *str)
 {
-	int indx, sgn, nmb, lnt, fd, digit;
+    int indx, sgn, nmb, lnt, digit;
 
-	indx = 0;
-	sgn = 0;
-	nmb = 0;
-	lnt = 0;
-	fd = 0;
-	digit = 0;
+    indx = 0;
+    sgn = 0;
+    nmb = 0;
+    lnt = 0;
+    digit = 0;
 
-	while (str[lnt] != '\0')
-		lnt++;
+    while (str[lnt] != '\0')
+        lnt++;
 
-	while (indx < lnt && sgn == 0)
-	{
-		if (str[indx] == '-')
-			++sgn;
+    while (indx < lnt && sgn == 0)
+    {
+        if (str[indx] == '-')
+            ++sgn;
 
-		if (str[indx] >= '0' && str[indx] <= '9')
-		{
-			digit = str[indx] - '0';
-			if (sgn % 2)
-				digit = -digit;
-			nmb = nmb * 10 + digit;
-			sgn = 1;
-			if (str[indx + 1] < '0' || str[indx + 1] > '9')
-				break;
-			sgn = 0;
-		}
-		indx++;
-	}
+        if (str[indx] >= '0' && str[indx] <= '9')
+        {
+            digit = str[indx] - '0';
+            if (sgn % 2)
+                digit = -digit;
+            nmb = nmb * 10 + digit;
+            sgn = 1;
+            if (str[indx + 1] < '0' || str[indx + 1] > '9')
+                break;
+            sgn = 0;
+        }
+        indx++;
+    }
 
-	if (sgn == 0)
-		return 0;
+    if (sgn == 0)
+        return 0;
 
-	return nmb;
+    return nmb;
+}
 }
